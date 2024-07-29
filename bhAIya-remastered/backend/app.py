c4d69184b0173e0f564a643e7ad1bd1e33e0a87b
Rachit Das <88940647+Exterminator11@users.noreply.github.com>
2024-07-30 00:28:27 +0530

    added basic mongo functionality

--- bhAIya-remastered/backend/app.py
+++ bhAIya-remastered/backend/app.py
@@ -5,11 +5,28 @@ from similarity import find_top_k_similar
 import json
 import os 
 from dotenv import load_dotenv
+from pymongo import MongoClient
+import sys
 
 load_dotenv()
 
 database_path = os.getenv("SAMPLE_DATABASE_NORMAL")
 images_path = os.getenv("SAMPLE_DATABASE_IMAGE")
+
+mongoDatabase=MongoClient(os.getenv("CONNECTION_STRING"))["bhAIya"]
+
+try:
+    database = mongoDatabase["database"].find({}, {"_id": 0})
+    database=list(database)
+    print("Data loaded")
+except Exception as e:
+    print("Error loading main database")
+try:
+    imgDatabase = mongoDatabase["imageDatabase"].find({}, {"_id": 0})
+    imgDatabase=list(imgDatabase)
+    print("Image database loaded")
+except Exception as e:
+    print("Erorr loading image database")
 
 app=FastAPI()
 @app.get("/")
@@ -18,20 +35,20 @@ async def root():
 
 @app.post("/data")
 async def data(data:dict):
-    database=None
-    imgDatabase=None
+    # database=None
+    # imgDatabase=None
     textCategories=None
     imgCategories=None
-    try:
-        with open(database_path,"r") as f:
-            database=json.load(f)
-    except Exception as e:
-        print(f"An error occured while reading the database: {e}")
-    try:
-        with open(images_path,"r") as f:
-            imgDatabase=json.load(f)
-    except Exception as e:
-        print(f"An error occured while reading the image database: {e}")
+    # try:
+    #     with open(database_path,"r") as f:
+    #         database=json.load(f)
+    # except Exception as e:
+    #     print(f"An error occured while reading the database: {e}")
+    # try:
+    #     with open(images_path,"r") as f:
+    #         imgDatabase=json.load(f)
+    # except Exception as e:
+    #     print(f"An error occured while reading the image database: {e}")
     text=data.get("text",None)
     img64=data.get("img64",None)
     categories={}
@@ -55,6 +72,103 @@ async def data(data:dict):
         )
         l.append(result[1])
     return l
+
+@app.post("/addOne")
+async def addOne(data:dict):
+    """
+      Payload
+      {"data":{
+                  "id":0,
+                  "price":123,
+                  "Main category":[],
+                  "Sub categories":[],
+                  "Additional details":[]
+              },
+              "imgData":{
+                    "id":0,
+                    "image":"b'/9j/4AAQSk'"
+                }
+      }
+    """
+    if(data.get("data",None)):
+        print("inserting into main database")
+        try:
+            x=mongoDatabase["database"].insert_one(data["data"])
+            print(x)
+        except Exception as e:
+            print(e)
+            print("Error inserting into main database")
+
+    if(data.get("imgData",None)):
+        print("inserting into image database")
+        try:
+            x=mongoDatabase["imageDatabase"].insert_one(data["imgData"])
+            print(x)
+        except Exception as e:
+            print("Error inserting into image database")
+
+
+@app.post("/removeOne")
+async def removeOne(id:int):
+    '''
+    Payload : 0(any integer, id to be removed)
+    '''
+    print("deleting from database")
+    try:
+        x = mongoDatabase["database"].delete_one({"id": id})
+        print(x)
+    except Exception as e:
+        print("Error deleting from main database")
+
+    try:
+        x=mongoDatabase["imageDatabase"].delete_one({"id":id})
+        print(x)
+    except Exception as e:
+        print("Error deleting image database")
+
+@app.post("/addMany")
+async def addMany(data:dict):
+    """
+        payload
+        {
+            "data":[{
+                "id":0,
+                "price":123,
+                "Main category":[],
+                "Sub categories":[],
+                "Additional details":[]
+            },{
+                "id":0,
+                "price":123,
+                "Main category":[],
+                "Sub categories":[],
+                "Additional details":[]
+            }],
+            "imgData":[{
+                "id":0,
+                "image":"b'/9j/4AAQSk'"
+
+            },{
+                "id":0,
+                "image":"b'/9j/4AAQSk'"
+
+            }]
+        }
+    """
+    if(data.get("data",None)):
+        print("inserting many into database")
+        try:
+            x = mongoDatabase["database"].insert_many(data.get("data"))
+            print(x)
+        except Exception as e:
+            print("Error inserting many into main database")
+
+    if(data.get("imgData",None)):
+        try:
+            x = mongoDatabase["imageDatabase"].insert_many(data.get("imgData"))
+            print(x)
+        except Exception as e:
+            print("Error inserting many into image database")
 
 
 if __name__=="__main__":
