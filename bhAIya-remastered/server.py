1622f7cac093b425886ef17ec86e0844ee006075
Rachit Das <88940647+Exterminator11@users.noreply.github.com>
2024-08-02 00:17:59 +0530

    admin dasboard functionality added

--- bhAIya-remastered/server.py
+++ bhAIya-remastered/server.py
@@ -14,7 +14,7 @@ from datetime import datetime
 import logging
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv("/Users/rachitdas/Desktop/newBhaiya/Bhaiya/bhAIya-remastered/backend/.env")
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Set a secret key for session management
